c2f3897f6f148c155503ea07153afb5de19faea8
Mykola Yefremov <efremovnikolaus@gmail.com>
2025-01-05 17:53:47 +0100

    Initialization commit/template

---
+++ src/main/java/com/rtap/EventCollectorServiceApplication.java
@@ -0,0 +1,12 @@
+package com.rtap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventCollectorServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EventCollectorServiceApplication.class, args);
+  }
+}
