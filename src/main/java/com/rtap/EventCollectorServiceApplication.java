f857ba3d62698ce8c4627cd4d4d868cf51f3ccbf
Mykola Yefremov <efremovnikolaus@gmail.com>
2025-01-05 18:24:31 +0100

    **fix build.gradle: improve readability and fix tasks.named() issue; feat: qodana.yml impl**

--- src/main/java/com/rtap/EventCollectorServiceApplication.java
+++ src/main/java/com/rtap/EventCollectorServiceApplication.java
@@ -3,6 +3,10 @@ package com.rtap;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The EventCollectorServiceApplication class serves as the entry point for the Spring Boot application.
+ * It initializes and launches the application by invoking the SpringApplication.run() method.
+ */
 @SpringBootApplication
 public class EventCollectorServiceApplication {
 
